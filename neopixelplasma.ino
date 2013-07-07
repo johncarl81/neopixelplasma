b5d26ab2deee54a7fce068e9dc38a4d8ab59171a
John Ericksen <johncarl81@gmail.com>
2013-07-06 22:27:22 -0600

    Added license and readme

--- neopixelplasma.ino
+++ neopixelplasma.ino
@@ -1,3 +1,21 @@
+/*
+    Multicolored Plasma for the Arduino Micro-Controller and NeoPixel Shield
+    Copyright (C) 2013 John Ericksen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
 #include <Adafruit_NeoPixel.h>
 
 // Parameter 1 = number of pixels in strip
