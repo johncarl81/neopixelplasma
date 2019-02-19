388d755acd02a26d6de49dd3e8b3c8f231b17399
John Ericksen <johncarl81@gmail.com>
2019-02-19 15:50:54 -0700

    Updated copyright

--- neopixelplasma.ino
+++ neopixelplasma.ino
@@ -1,6 +1,6 @@
 /*
     Multicolored Plasma for the Arduino Micro-Controller and NeoPixel Shield
-    Copyright (C) 2013 John Ericksen
+    Copyright (C) 2019 John Ericksen
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
